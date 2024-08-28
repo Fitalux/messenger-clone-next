568e7062ef74695cca291593d5bdf56c8d075980
Fitalux <goed0522@gmail.com>
2024-08-28 23:36:36 +0900

    auth Ins

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import ToasterContext from "./context/ToasterContext";
+import AuthContext from "./context/AuthContext";
+
+import ActiveStatus from "./components/ActiveStatus";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Messenger Clone",
+  description: "Messenger Clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthContext>
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
+        </AuthContext>
+      </body>
+    </html>
+  );
+}
